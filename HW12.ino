166732441f60766f15d2aec1c81730ac458fd617
Lauren Tsao <et2676@nyu.edu>
2024-11-27 20:49:22 -0500

    added components

--- HW12.ino
+++ HW12.ino
@@ -1,9 +1,57 @@
-void setup() {
-  // put your setup code here, to run once:
+int nClicks;
+int pv2;
 
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  pinMode(2, INPUT); // Doesn't put out any energy on the pin
+
+Serial.begin(9600);
+
+pv2 = 0;
+nClicks = 0;
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  // digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)   
+  // digitalWrite(11, HIGH);  // turn the LED on (HIGH is the voltage level)
+  // digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
+
+
+// button
+  int v2 = digitalRead(2); //read whatever that is on pin 2 and put in that variable;
+
+  if(v2 == 1 && pv2 == 0) {
+    nClicks += 1;
+  }
+
+  pv2 = v2; //update the previous value of v2
+
+  // Serial.println("hello"); // print hello and go on to a new line
+  Serial.println("v2:" + String(v2) + "count" + String(nClicks));
+
+  delay(1);
+
+// photosensor
+  int a0In = analogRead(A0);
+
+  Serial.println(a0In);
+
+  delay(100);
+
+
+
+// rotary
+int a1In = analogRead(A1);
+
+  Serial.println(a1In);
+
+  delay(100);
+
+
+  
 
 }
