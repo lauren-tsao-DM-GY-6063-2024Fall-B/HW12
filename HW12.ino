dff434337210051c14b081a8f1e700718a436224
Lauren Tsao <et2676@nyu.edu>
2024-11-27 21:14:13 -0500

    all components coded in

--- HW12.ino
+++ HW12.ino
@@ -1,5 +1,7 @@
 int nClicks;
 int pv2;
+bool led12On = false;
+bool led11On = false;
 
 void setup() {
   pinMode(12, OUTPUT);
@@ -16,9 +18,6 @@ nClicks = 0;
 
 // the loop function runs over and over again forever
 void loop() {
-  // digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)   
-  // digitalWrite(11, HIGH);  // turn the LED on (HIGH is the voltage level)
-  // digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
 
 
 // button
@@ -33,12 +32,26 @@ void loop() {
   // Serial.println("hello"); // print hello and go on to a new line
   Serial.println("v2:" + String(v2) + "count" + String(nClicks));
 
-  delay(1);
+  // If the button has been pressed 3 times, turn on pin 12
+  if (nClicks >= 5 && !led12On) {
+    digitalWrite(12, HIGH);  // Turn on pin 12 (LED)
+    Serial.println("Pin 12 UNLOCKED!");  // Print message for feedback
+    led12On = true;  // Set the LED status to "on"
+  }
+
+  delay(100);
 
 // photosensor
   int a0In = analogRead(A0);
 
   Serial.println(a0In);
+
+if (a0In == 0) {
+    digitalWrite(11, HIGH);  // Turn on pin 11 (LED)
+    Serial.println("Pin 11 UNLOCKED!");
+  } else {
+    digitalWrite(11, LOW);  // Turn off pin 11 if the value is not 0
+  }
 
   delay(100);
 
@@ -48,6 +61,13 @@ void loop() {
 int a1In = analogRead(A1);
 
   Serial.println(a1In);
+
+  if (a1In >= 1000 && a1In < 2000) {
+    digitalWrite(10, HIGH);  // Turn on pin 10 (LED)
+    Serial.println("Pin 10 UNLOCKED!");
+  } else {
+    digitalWrite(10, LOW);  // Turn off pin 10 if the value is less than 1010
+  }
 
   delay(100);
 
