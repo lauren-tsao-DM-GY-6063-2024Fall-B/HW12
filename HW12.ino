ad2f079a3178345e0269f54e11d26cb1ddfd1123
Lauren Tsao <et2676@nyu.edu>
2024-11-29 17:13:06 -0500

    finalized code

--- HW12.ino
+++ HW12.ino
@@ -1,96 +1,104 @@
-int nClicks;
-int pv2;
+int pv2;  // previous value of pin 2
+int nClicks; // number of clicks
+
 bool led12On = false;
 bool led11On = false;
 bool led10On = false;
 
 void setup() {
-  pinMode(12, OUTPUT);
+  pinMode(12, OUTPUT); // receives messages from Arduino
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
 
-  pinMode(2, INPUT); // Doesn't put out any energy on the pin
+  pinMode(2, INPUT); // sends messages to Arduino
 
+// initiate serial communication between Arduino board and computer
+// Serial = allows sending and receiving data to and from Arduino via USB port, typically using the Serial.print(), Serial.println(), Serial.read()
+// begin = start serial communication
+// 9600 = baud rate (defines the speed the data is transmitted in bits/bauds pr second (bps)), so this is 9600 bps
 Serial.begin(9600);
 
 pv2 = 0;
-nClicks = 0;
+nClicks = 0; 
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
 
+// BUTTON
+  int v2 = digitalRead(2); // read input from pin 2 and put it in v2 variable;
 
-// button
-  int v2 = digitalRead(2); //read whatever that is on pin 2 and put in that variable;
-
-  if(v2 == 1 && pv2 == 0) {
-    nClicks += 1;
+  if(v2 == 1 && pv2 == 0) { // if current value of pin 2 is 1 (HIGH)(pressed) and previous value of pin 2 is 0 (LOW)(released)..
+    nClicks += 1; // .. increase nClicks counter by 1
   }
 
-  pv2 = v2; //update the previous value of v2
+  pv2 = v2; // update the previous value of pin 2 (pv2) to current value of pin 2 (v2)
 
-  // Serial.println("hello"); // print hello and go on to a new line
+  // print the following line everytime the above is executed
+  // "v2:" = title for v2's value (String(v2))
+  // String(v2) = converts value of v2 (either 1 or 0 dependant on pin 2's state (HIGH/LOW)) into a string
+  // "count" = another title, for number of clicks (String(nClicks))
+  // String(nClicks) = converts nClicks into a string
+  // so if button is pressed 3 times, it should read as "v2:1count3"
   Serial.println("v2:" + String(v2) + "count" + String(nClicks));
 
-  // If the button has been pressed 3 times, turn on pin 12
-  if (nClicks >= 5 && !led12On) {
-    digitalWrite(12, HIGH);  // Turn on pin 12 (LED)
-    Serial.println("Pin 12 UNLOCKED!");  // Print message for feedback
-    led12On = true;  // Set the LED status to "on"
-  }
-
-  delay(100);
-
-// photosensor
-  int a0In = analogRead(A0);
-
-  Serial.println(a0In);
-
-if (a0In == 0) {
-    digitalWrite(11, HIGH);  // Turn on pin 11 (LED)
-    Serial.println("Pin 11 UNLOCKED!");  // Print message for feedback
-    led11On = true;  // Set the LED status to "on"
-  } else {
-    led11On = false;  // Keep track of pin 11 state
+  // if the button has been pressed 5 times, turn on LED on pin 12
+  if (nClicks == 5 && !led12On) { // if number of clicks is equals to 5 and LED on pin 12 is not on
+    digitalWrite(12, HIGH);  // Turn on LED on pin 12
+    Serial.println("Pin 12 UNLOCKED!");  // print "Pin 12 UNLOCKED" as visual feedback
+    led12On = true;  // ensure that LED on pin 12 is unlocked only when button is pressed 5 times, and prevents LED from being turned on again from subsequent presses
   }
 
   delay(100);
 
 
+// PHOTORESISTOR
+  int a0In = analogRead(A0); // read value from analog pin A0..
 
-// rotary
-int a1In = analogRead(A1);
+  Serial.println(a0In); // .. and print it on the Serial Monitor
 
-  Serial.println(a1In);
-
-  if (a1In >= 1000 && a1In < 2000) {
-    digitalWrite(10, HIGH);  // Turn on pin 10 (LED)
-    Serial.println("Pin 10 UNLOCKED!");  // Print message for feedback
-    led10On = true;  // Set the LED status to "on"
-  } else {
-    led10On = false;  // Keep track of pin 10 state
+if (a0In == 0) { // if reading is 0..
+    digitalWrite(11, HIGH);  // .. turn on LED on pin 11
+    Serial.println("Pin 11 UNLOCKED!"); // print "Pin 11 UNLOCKED" as visual feedback
+    led11On = true;
+  } else { // otherwise.. 
+    led11On = false;  // .. leave LED on pin 11 off
   }
 
   delay(100);
 
 
+// ROTARY POTENTIOMETER
+int a1In = analogRead(A1); // read value from analog pin A1..
 
- // Check if all LEDs are on (button, photosensor, rotary)
+  Serial.println(a1In); // .. and print it on the Serial Monitor
+
+  if (a1In >= 1000 && a1In < 2000) { // if the reading ranges between 1000 and 2000..
+    digitalWrite(10, HIGH);  // .. turn on LED on pin 10
+    Serial.println("Pin 10 UNLOCKED!");  // print "Pin 10 UNLOCKED" as visual feedback
+    led10On = true;
+  } else { // otherwise..
+    led10On = false;  // .. leave LED on pin 10 off
+  }
+
+  delay(100);
+
+//////
+
+ // check if all LEDs on pins 10, 11 and 12 are on 
   if (led12On && led11On && led10On) {
-    // Blink the LEDs forever
-    while (true) {  // Infinite loop
-      // Turn on all LEDs
+    // blink the LEDs forever
+    while (true) {  // creates an infinte loop (because true will always be true) (not related to the 'true' statuses of the pins 10-12) 
+      // make all LEDs blink (turn on(HIGH) and off(LOW) at 500ms intervals)
       digitalWrite(12, HIGH);
       digitalWrite(11, HIGH);
       digitalWrite(10, HIGH);
-      delay(500);  // Wait for half a second
+      delay(500); 
 
-      // Turn off all LEDs
       digitalWrite(12, LOW);
       digitalWrite(11, LOW);
       digitalWrite(10, LOW);
-      delay(500);  // Wait for half a second
+      delay(500);
     }
   }
 }
